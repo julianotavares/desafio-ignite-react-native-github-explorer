6a9fbfd4a752cf8f4a6d95e61b75c37705a4139b
Juliano Tavares <juliano.tavaress@gmail.com>
2021-09-30 18:32:09 -0300

    fix test

--- src/components/Card/CardAnimation/index.tsx
+++ src/components/Card/CardAnimation/index.tsx
@@ -20,13 +20,25 @@ export function CardAnimation({ children, ...rest }: CardAnimationProps) {
   const animatedStyle = useAnimatedStyle(() => {
     return {
       opacity: cardOpacity.value,
-      transform: [{ translateX: cardOffset.value }],
+      transform: [
+        {
+          translateX: cardOffset.value
+        }
+      ],
     }
   })
 
   useEffect(() => {
-    cardOpacity.value = withTiming(1, { duration: 1000 });
-    cardOffset.value = withTiming(9, { duration: 1000 });
+   
+     cardOpacity.value = withTiming(
+      1,
+      {duration: 1000},
+      );
+
+      cardOffset.value = withTiming(
+        0,
+        {duration: 1000},
+        );
   }, []);
 
   return (
